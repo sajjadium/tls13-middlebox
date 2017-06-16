85549e88dfebf68d132dd5cb3b468e1402104aa1
Sajjad Arshad <arshad@ccs.neu.edu>
2017-06-16 15:24:29 -0700

    check all the combination of max version and fallback limit

--- add-on/bootstrap.js
+++ add-on/bootstrap.js
@@ -7,6 +7,25 @@ Cu.import("resource://gre/modules/TelemetryController.jsm");
 
 let prefs = new Preferences({defaultBranch: true});
 
+let configurations = [
+    {max_version: 4, fallback_limit: 4, is_tls13: true, website: "enabled.tls13.com"},
+    {max_version: 4, fallback_limit: 4, is_tls13: false, website: "disabled.tls13.com"},
+    {max_version: 4, fallback_limit: 3, is_tls13: true, website: "www.allizom.org"},
+    {max_version: 4, fallback_limit: 3, is_tls13: false, website: "control.tls12.com"},
+    {max_version: 3, fallback_limit: 3, is_tls13: true, website: "enabled.tls13.com"},
+    {max_version: 3, fallback_limit: 3, is_tls13: false, website: "short.tls13.com"}
+];
+
+async function testConfigurations() {
+    let output = [];
+
+    for (let config of configurations) {
+        output.push(await makeRequest(config));
+    }
+
+    return output;
+}
+
 function getError(xhr) {
     let result = {};
 
@@ -19,11 +38,10 @@ function getError(xhr) {
             secInfo.QueryInterface(Ci.nsITransportSecurityInfo);
 
             result.securityState = secInfo.securityState;
+            result.errorMessage = secInfo.errorMessage;
 
-            if ((secInfo.securityState & Ci.nsIWebProgressListener.STATE_IS_BROKEN) === Ci.nsIWebProgressListener.STATE_IS_BROKEN) {
-                result.shortSecurityDescription = secInfo.shortSecurityDescription;
-                result.errorMessage = secInfo.errorMessage;
-            }
+            // For secure connections (SSL), this gives the common name (CN) of the certifying authority (Privacy Concern?)
+            result.shortSecurityDescription = secInfo.shortSecurityDescription;
         }
     } catch(err) {
         result.exception = err.message;
@@ -32,52 +50,50 @@ function getError(xhr) {
     return result;
 }
 
-function checkTLS(version, url) {
+function makeRequest(config) {
     return new Promise(function(resolve, reject) {
-        try {
-            prefs.set("security.tls.version.max", version);
-            prefs.set("security.tls.version.fallback-limit", version);
+        function reportResult(origin, xhr) {
+            let result = Object.assign({origin: origin}, config);
 
-            let request = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance(Ci.nsIXMLHttpRequest);
+            if (origin !== "load")
+                result = Object.assign(result, getError(xhr));
 
-            request.open("GET", url, true);
-
-            request.timeout = 10000;
-
-            request.channel.loadFlags |= Ci.nsIRequest.LOAD_ANONYMOUS;
-            request.channel.loadFlags |= Ci.nsIRequest.LOAD_BYPASS_CACHE;
-            request.channel.loadFlags |= Ci.nsIRequest.INHIBIT_CACHING;
-
-            request.addEventListener("load", function(e) {
-                let result = getError(e.target);
-                result.origin = "load";
-                resolve(result);
-            }, false);
-
-            request.addEventListener("error", function(e) {
-                let result = getError(e.target);
-                result.origin = "error";
-                resolve(result);
-            }, false);
-
-            request.addEventListener("abort", function(e) {
-                let result = getError(e.target);
-                result.origin = "abort";
-                resolve(result);
-            }, false);
-
-            request.addEventListener("timeout", function(e) {
-                let result = getError(e.target);
-                result.origin = "timeout";
-                resolve(result);
-            }, false);
-
-            request.send();
-        } catch (err) {
-            let result = {};
-            result.origin = "exception";
-            result.exception = err.message;
             resolve(result);
+        }
+
+        try {
+            prefs.set("security.tls.version.max", config.max_version);
+            prefs.set("security.tls.version.fallback-limit", config.fallback_limit);
+
+            let xhr = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance(Ci.nsIXMLHttpRequest);
+
+            xhr.open("GET", "https://" + config.website, true);
+
+            xhr.timeout = 10000;
+
+            xhr.channel.loadFlags |= Ci.nsIRequest.LOAD_ANONYMOUS;
+            xhr.channel.loadFlags |= Ci.nsIRequest.LOAD_BYPASS_CACHE;
+            xhr.channel.loadFlags |= Ci.nsIRequest.INHIBIT_CACHING;
+
+            xhr.addEventListener("load", function(e) {
+                reportResult("load", e.target);
+            }, false);
+
+            xhr.addEventListener("error", function(e) {
+                reportResult("error", e.target);
+            }, false);
+
+            xhr.addEventListener("abort", function(e) {
+                reportResult("abort", e.target);
+            }, false);
+
+            xhr.addEventListener("timeout", function(e) {
+                reportResult("timeout", e.target);
+            }, false);
+
+            xhr.send();
+        } catch (err) {
+            resolve(Object.assign({origin: "exception", exception: err.message}, config));
         }
     });
 }
@@ -90,13 +106,13 @@ function install() {
     let current_max = prefs.get("security.tls.version.max");
     let current_fallback = prefs.get("security.tls.version.fallback-limit");
 
-    checkTLS(4, "https://enabled.tls13.com").then(function(error4) {
-        checkTLS(3, "https://enabled.tls13.com").then(function(error3) {
-            TelemetryController.submitExternalPing("tls13-middlebox", {"1.3": error4, "1.2": error3});
+    testConfigurations().then(result => {
+        prefs.set("security.tls.version.max", current_max);
+        prefs.set("security.tls.version.fallback-limit", current_fallback);
 
-            prefs.set("security.tls.version.max", current_max);
-            prefs.set("security.tls.version.fallback-limit", current_fallback);
-        });
+        TelemetryController.submitExternalPing("tls13-middlebox", result);
+
+        console.log(result);
     });
 }
 
