af440ef6cff071a176af78b3ca89455ad209aaf6
Sajjad Arshad <arshad@ccs.neu.edu>
2017-07-20 15:02:44 -0700

    finalized the error codes

--- analysis/fix-numbers.py
+++ analysis/fix-numbers.py
@@ -2,11 +2,18 @@
 
 import sys
 
+l = []
+
 for line in sys.stdin:
-    if '*' in line or line.strip() == "":
-        continue
+#    if '*' in line or line.strip() == "":
+#        continue
 
-    tokens = line.strip().replace("(", "").replace(")", "").replace(",", "").split()
+#    tokens = line.strip().replace("(", "").replace(")", "").replace(",", "").split()
 
-    print "%s\t%s" % (tokens[0], hex(-0x3000 + int(tokens[-1])).upper())
+#    print "%s\t%s" % (tokens[0], hex(-0x3000 + int(tokens[-1])).upper())
+
+    l.append(line.strip().split())
+
+for x, y in sorted(l, key=lambda z: (int(z[1], 16), z[0])):
+    print '%s\t%s' % (y.lower(), x)
 
